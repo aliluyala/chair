35211851e58c4977354b2e55af3008f9fc5710ae
姚宇明 <yaoyuming@yaoyumingdeMacBook-Pro.local>
2017-04-15 21:18:51 +0800

    修改redis配置文件

--- chair-manager/src/main/java/com/chair/manager/controller/UsersController.java
+++ chair-manager/src/main/java/com/chair/manager/controller/UsersController.java
@@ -12,7 +12,6 @@ import com.chair.manager.bean.ReqParam;
 import com.chair.manager.bean.ResponseResult;
 import com.chair.manager.pojo.ConsumedDetails;
 import com.chair.manager.pojo.RechargeRecord;
-import com.chair.manager.redis.RedisClientTemplate;
 import com.chair.manager.service.ConsumePackageService;
 import com.chair.manager.service.ConsumedDetailsService;
 import com.chair.manager.service.RechargeRecordService;
@@ -34,9 +33,6 @@ public class UsersController {
 	private UsersService  usersService;
 	@Autowired
 	private ConsumePackageService  consumePackageService;
-	
-	@Autowired
-	private RedisClientTemplate redisClientTemplate;
 
 	@Autowired
 	private JedisCluster jedisCluster;
@@ -109,10 +105,9 @@ public class UsersController {
 	@ResponseBody
 	@RequestMapping(value="testRedis",method=RequestMethod.POST)
 	private ResponseResult TestRedis(@RequestBody ReqParam param){
-		System.out.println("---redisClientTemplate---"+redisClientTemplate);
 		System.out.println("----"+param);
-		String rs1 = jedisCluster.set("aaa", "4321");
-		String rs2 = jedisCluster.get("aaa");
+		String rs1 = jedisCluster.setex("bbb", 30, "12345555");
+		String rs2 = jedisCluster.get("bbb");
 //		String rs2 = redisService.set("13", "4321", 10);
 		System.err.println("---rs1---"+rs1);
 		System.err.println("---rs2---"+rs2);
