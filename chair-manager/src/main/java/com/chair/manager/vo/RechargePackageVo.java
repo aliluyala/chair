a1cf169a084aa62086c08a01e1e8c81bf32e1d00
姚宇明 <yaoyuming@yaoyumingdeMacBook-Pro.local>
2017-04-26 23:34:19 +0800

    新增查询充值套餐列表方法

---
+++ chair-manager/src/main/java/com/chair/manager/vo/RechargePackageVo.java
@@ -0,0 +1,64 @@
+package com.chair.manager.vo;
+
+import java.io.Serializable;
+import java.util.List;
+
+/**
+ * @author yaoyuming
+ *
+ */
+public class RechargePackageVo implements Serializable {
+	private static final long serialVersionUID = 1L;
+
+	private int packageID;
+
+	private String packageName;
+
+	private String rechargeAmount;
+
+	private String rechargeDuration;
+
+	private List<RechargePackageVo> packageList;
+
+	public int getPackageID() {
+		return packageID;
+	}
+
+	public void setPackageID(int packageID) {
+		this.packageID = packageID;
+	}
+
+	public String getPackageName() {
+		return packageName;
+	}
+
+	public void setPackageName(String packageName) {
+		this.packageName = packageName;
+	}
+
+	public String getRechargeAmount() {
+		return rechargeAmount;
+	}
+
+	public void setRechargeAmount(String rechargeAmount) {
+		this.rechargeAmount = rechargeAmount;
+	}
+
+	public String getRechargeDuration() {
+		return rechargeDuration;
+	}
+
+	public void setRechargeDuration(String rechargeDuration) {
+		this.rechargeDuration = rechargeDuration;
+	}
+
+	public List<RechargePackageVo> getPackageList() {
+		return packageList;
+	}
+
+	public void setPackageList(List<RechargePackageVo> packageList) {
+		this.packageList = packageList;
+	}
+
+
+}
