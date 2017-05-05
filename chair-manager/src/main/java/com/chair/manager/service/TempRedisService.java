b661a224c00a6479e3310ef7fb728e417f770677
姚宇明 <yaoyuming@yaoyumingdeMacBook-Pro.local>
2017-05-05 22:18:55 +0800

    i

---
+++ chair-manager/src/main/java/com/chair/manager/service/TempRedisService.java
@@ -0,0 +1,12 @@
+package com.chair.manager.service;
+
+
+import org.springframework.stereotype.Service;
+
+import com.chair.manager.pojo.TempRedis;
+
+@Service
+public class TempRedisService extends BaseService<TempRedis> {
+	
+
+}
