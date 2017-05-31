032b947026fa0677d4a42550281045349204afa1
liuzhiwei <572361524@qq.com>
2017-05-31 10:09:14 +0800

    修改登录拦截器代码

--- chair-manager/src/main/java/com/chair/manager/interceptor/UserInterceptor.java
+++ chair-manager/src/main/java/com/chair/manager/interceptor/UserInterceptor.java
@@ -3,17 +3,23 @@ package com.chair.manager.interceptor;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
+import org.apache.log4j.Logger;
 import org.springframework.web.servlet.HandlerInterceptor;
 import org.springframework.web.servlet.ModelAndView;
 
 public class UserInterceptor implements HandlerInterceptor {
 
+	
+	private static Logger LOGGER = Logger.getLogger(UserInterceptor.class);
 	@Override
 	public boolean preHandle(HttpServletRequest request,
 			HttpServletResponse response, Object handler) throws Exception {
 		if(request.getSession().getAttribute("user")!=null){
 			return true;
 		}
+		String url="/"+request.getRequestURI().split("/")[1]+"/login.jsp";
+		LOGGER.debug("登录失败超时\t跳转页面url="+url);
+		response.sendRedirect(url);
 		return false;
 	}
 
@@ -21,16 +27,14 @@ public class UserInterceptor implements HandlerInterceptor {
 	public void postHandle(HttpServletRequest request,
 			HttpServletResponse response, Object handler,
 			ModelAndView modelAndView) throws Exception {
-		// TODO Auto-generated method stub
-
+		LOGGER.debug(request.getParameter("user")+"登录失败----postHandle");
 	}
 
 	@Override
 	public void afterCompletion(HttpServletRequest request,
 			HttpServletResponse response, Object handler, Exception ex)
 					throws Exception {
-		// TODO Auto-generated method stub
-
+		LOGGER.debug(request.getParameter("user")+"登录失败----afterCompletion");
 	}
 
 }
